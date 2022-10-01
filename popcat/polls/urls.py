6dfdedcee8cf7acdd02a4d6f120f45948a40a9fb
hurdoo <64835865+HURDOO@users.noreply.github.com>
2022-10-01 13:37:17 +0900

    Update HTML to django format

--- popcat/polls/urls.py
+++ popcat/polls/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.start),
+    path('count', views.count),
+    path('win', views.win)
 ]
--- popcat/polls/views.py
+++ popcat/polls/views.py
@@ -5,3 +5,15 @@ def index(request):
     return render(request, 'polls/index.html', {
 
     })
+
+
+def count(request):
+    return render(request, 'polls/count.html')
+
+
+def start(request):
+    return render(request, 'polls/start.html')
+
+
+def win(request):
+    return render(request, 'polls/win.html')
